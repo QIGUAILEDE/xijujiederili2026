56fb6f007d5ab68d2f178752ae7915a73879e37c
Jianzhe <yangjianzhe@icloud.com>
2026-03-06 15:57:04 +0800

    咿呀呀

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/xijujiederili2026/', // 这里的名字必须和您的仓库名字一模一样
+  base: '/xijujiederili2026/', // 👉 这行极其重要！开头和结尾都必须有斜杠！
 })
